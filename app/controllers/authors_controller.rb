4904a21f359b60901ba0888e3d9a1e91ba7d0eed
BaronLip <baronlip@gmail.com>
2019-09-23 16:57:27 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,23 @@
 class AuthorsController < ApplicationController
 
-  def show
-    @author = Author.find(params[:id])
-  end
+    def show
+        @author = Author.find(params[:id])
+    end
 
+    # No long need code below after nested routes and conditional in posts_controller.
+    
+    # def posts_index
+    #     @author = Author.find(params[:id])
+    #     @posts = @author.posts
+    #     render template: 'posts/index'
+    # end
+
+    # def post
+    #    @author = Author.find(params[:id])
+    
+    #     # Note that because ids are unique by table we can go directly to
+    #     # Post.find — no need for @author.posts.find...
+    #     @post = Post.find(params[:post_id])
+    #     render template: 'posts/show' 
+    # end
 end
